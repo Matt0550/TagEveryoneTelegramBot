2d3a9c033097fc01eb81346dbf27cc9efe5c6562
Matt0550 <65899974+Matt0550@users.noreply.github.com>
2022-04-18 21:40:38 +0200

    [+] Added cooldown system

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+import re
 from telegram.ext.updater import Updater
 from telegram.update import Update
 from telegram.ext.callbackcontext import CallbackContext
@@ -6,15 +7,45 @@ from telegram.ext.messagehandler import MessageHandler
 from telegram.ext.filters import Filters
 from database import Database
 import json
-import random
+import datetime
 # import os
 # from keep_alive import keep_alive
 
 # Create an istance of database
 db = Database()
+OWNER_ID = 313344916
+
+# Create a decorator to apply cooldown to a function (in seconds) for user who used the command
+def cooldown(seconds):
+    def decorator(func):
+        # Create a dictionary to store the last time the user used the command
+        last_time = {}
+
+        def wrapper(update: Update, context: CallbackContext):
+            # Get the user id
+            user_id = update.message.from_user.id
+            # Get the current time
+            now = datetime.datetime.now()
+            # Check if the user has used the command before
+            if user_id in last_time:
+                # Check if the user has used the command in the last seconds
+                if now - last_time[user_id] < datetime.timedelta(seconds=seconds):
+                    # If the user has used the command in the last seconds, send a message to the user
+                    update.message.reply_text("You can use this command again in %s seconds" % str(seconds - (now - last_time[user_id]).seconds))
+                    # Return to avoid the function to be executed
+                    return
+            # Update the last time the user used the command
+            last_time[user_id] = now
+            # Execute the function
+            func(update, context)
+        return wrapper
+    return decorator
+
 # os.environ['token']
-updater = Updater("INSERT_TOKEN_HERE", use_context=True)
-  
+updater = Updater("5289537493:AAHb_jQOorFA3YZmN__cpqt9rchF7nkyu7M", use_context=True)
+start_time = datetime.datetime.now() # For uptime
+
+@cooldown(15)
 def start(update: Update, context: CallbackContext):
     # Check if bot is in a group
     if not update.message.chat.type == "group" or not update.message.chat.type == "supergroup":
@@ -30,6 +61,7 @@ def unknown(update: Update, context: CallbackContext):
     update.message.reply_text(
         "Sorry '%s' is not a valid command" % update.message.text)
 
+@cooldown(15)
 # Function to add a member to the list
 def join_list(update: Update, context: CallbackContext):
     try:
@@ -52,6 +84,7 @@ def join_list(update: Update, context: CallbackContext):
         # Print error with code markup
         update.message.reply_text("Error:\n`%s`" % e, parse_mode="Markdown")
 
+@cooldown(15)
 # Function to remove a member from the list
 def leave_list(update: Update, context: CallbackContext):
     try:
@@ -74,14 +107,10 @@ def leave_list(update: Update, context: CallbackContext):
         # Print error with code markup
         update.message.reply_text("Error:\n`%s`" % e, parse_mode="Markdown")
 
-words = ["gg", "cc", "gaddean", "gaddean bro", "gg bro", "cc bro"]
+@cooldown(15)
 def everyone(update: Update, context: CallbackContext):
-    # Check if message contains a word from the list
-    if update.message.text.lower() in words:
-        # Random response from words list
-        update.message.reply_text(words[random.randint(0, len(words) - 1)])
-    
-    if update.message.text == "@everyone" or update.message.text == "@all" or update.message.text == "/everyone" or update.message.text == "/all" or update.message.text == "@"+update.message.bot.username:
+    everyoneCommands = ["@everyone", "@all", "/everyone", "/all", "/everyone@"+update.message.bot.username, "/all@"+update.message.bot.username, "@"+update.message.bot.username]
+    if update.message.text in everyoneCommands:
         if update.message.chat.type == "group" or update.message.chat.type == "supergroup":
             # Check if bot is admin in the group
             if update.message.chat.get_member(updater.bot.id).status == "administrator":
@@ -103,15 +132,19 @@ def everyone(update: Update, context: CallbackContext):
                         data = data.replace("]", "")
                         
                         members = []
-                        # Foreach data in list
-                        for i in data.split(","):
-                            try:
-                                members.append("@" + update.message.chat.get_member(int(i)).user.username)
-                            except Exception as e:
-                                print("[USER] " + str(e) + " - " + str(i))
-                                continue
-                        # Send message with list of members
-                        update.message.reply_text("\n".join(members))
+                        try:
+                            # Foreach data in list
+                            for i in data.split(","):
+                                try:
+                                    members.append("@" + update.message.chat.get_member(int(i)).user.username)
+                                except Exception as e:
+                                    print("[USER] " + str(e) + " - " + str(i))
+                                    continue
+                            # Send message with list of members
+                            update.message.reply_text("\n".join(members))
+                        except Exception as e:
+                            print("[ERROR] " + str(e))
+                            update.message.reply_text("No one is in the list")
 
                 except Exception as e:
                     print("[ERROR] " + str(e))
@@ -122,20 +155,85 @@ def everyone(update: Update, context: CallbackContext):
         else:
             update.message.reply_text("This command is can only be used in a group")
 
+@cooldown(15)
 def help(update: Update, context: CallbackContext):
     update.message.reply_text("""
 /in - Add yourself to the Everyone's list\n
 /out - Remove yourself from the Everyone's list\n
-/everyone - Send a message to all in the list\n\n
-Developed by @Non_Sono_Matteo\n
+/everyone - Send a message to all in the list\n
+/help - Show this message\n
+/list - Show the list of everyone's list (without metion)\n
+/status - Show the status of the bot\n\n
+Developed by @Non_Sono_Matteo
 https://matt05.ml
 """)
+
+@cooldown(15)
+def getList(update: Update, context: CallbackContext):
+    if update.message.chat.type == "group" or update.message.chat.type == "supergroup":
+        # Check if bot is admin in the group
+        if update.message.chat.get_member(updater.bot.id).status == "administrator":
+            try: 
+                # Get the group id from the message
+                group_id = update.message.chat.id
+                # Get data from database
+                data = db.getMembers(group_id)
+                # Convert data to json
+                data = json.loads(data[0][0])
+                # Check if data is empty
+                if not data:
+                    update.message.reply_text("No one is in the list")
+                else:
+                    # Convert data to string
+                    data = str(data)
+                    # Remove "[" and "]"
+                    data = data.replace("[", "")
+                    data = data.replace("]", "")
+                    
+                    members = []
+                    try:
+                        # Foreach data in list
+                        for i in data.split(","):
+                            try:
+                                members.append(update.message.chat.get_member(int(i)).user.username + " - " + str(i))
+                            except Exception as e:
+                                print("[USER] " + str(e) + " - " + str(i))
+                                continue
+                        # Send message with list of members
+                        update.message.reply_text("\n".join(members))
+                    except Exception as e:
+                        print("[ERROR] " + str(e))
+                        update.message.reply_text("No one is in the list")
+
+            except Exception as e:
+                print("[ERROR] " + str(e))
+                # Print error with code markup
+                update.message.reply_text("Error:\n`%s`" % e, parse_mode="Markdown")
+        else:
+            update.message.reply_text("The bot must be admin to use this command in a group")
+    else:
+        update.message.reply_text("This command is can only be used in a group")
+
+@cooldown(15)
+def status(update: Update, context: CallbackContext):
+    # Get the bot uptime widout microseconds
+    uptime = datetime.datetime.now() - start_time
+    uptime = str(uptime).split(".")[0]
+
+    update.message.reply_text("✅ If you see this message, the bot is working\n⏰ Uptime: %s" % str(uptime))
+
+def sendMessageOwner(message):
+    # Send message to owner id
+    updater.bot.send_message(OWNER_ID, message)
+
 updater.dispatcher.add_handler(CommandHandler('start', start))
 updater.dispatcher.add_handler(CommandHandler('everyone', everyone))
 updater.dispatcher.add_handler(CommandHandler('all', everyone))
 updater.dispatcher.add_handler(CommandHandler('in', join_list))
 updater.dispatcher.add_handler(CommandHandler('out', leave_list))
 updater.dispatcher.add_handler(CommandHandler('help', help))
+updater.dispatcher.add_handler(CommandHandler('status', status))
+updater.dispatcher.add_handler(CommandHandler('list', getList))
 
 updater.dispatcher.add_handler(MessageHandler(Filters.text, everyone))
 
@@ -144,3 +242,5 @@ updater.dispatcher.add_handler(MessageHandler(Filters.command, unknown))  # Filt
 
 # keep_alive() # Replit hosting
 updater.start_polling()
+# On bot start, send message to owner id
+sendMessageOwner("✅ Bot started and ready to use") # Comment this line if you don't want to send a message to the owner when the bot starts
