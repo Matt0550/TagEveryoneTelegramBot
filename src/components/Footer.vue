5ef2d3ef8349ae2a02632dc40cee11faed2afb4b
Matt0550 <65899974+Matt0550@users.noreply.github.com>
2025-03-10 21:11:54 +0100

    moved

--- src/components/Footer.vue
+++ src/components/Footer.vue
@@ -1,7 +1,7 @@
 <script setup lang="ts">
-import Navbar from '../components/Navbar.vue';
-import Hero from '../components/Hero.vue';
-import Features from '../components/Features.vue';
+import Navbar from './Navbar.vue';
+import Hero from './Hero.vue';
+import Features from './Features.vue';
 
 // Anno corrente per il copyright
 const currentYear = new Date().getFullYear();
